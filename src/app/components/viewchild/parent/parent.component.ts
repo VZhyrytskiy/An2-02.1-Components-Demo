af1dec6322854737a0bcca29c373868144edaaec
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-02-06 16:06:54 +0200

    Fix ViewChild Demo

--- src/app/components/viewchild/parent/parent.component.ts
+++ src/app/components/viewchild/parent/parent.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 import { ChildComponent } from './../child/child.component';
 
@@ -7,19 +7,18 @@ import { ChildComponent } from './../child/child.component';
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements AfterViewInit {
 
   @ViewChild('input') inputField: ElementRef;
   @ViewChild(ChildComponent) child: ChildComponent;
   @ViewChild('child') childComp: ElementRef;
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     (<HTMLInputElement>this.inputField.nativeElement).value = 'Value From Parent';
     this.child.onClick();
 
     console.log(this.childComp);
   }
-
 
 }
