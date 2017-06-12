86d91fc5b47388c16c49a784ad5134187cb23708
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-06-12 16:03:09 +0300

    remove ElementRef

--- src/app/components/02-communication/02-output-event-directive/click-stop.directive.ts
+++ src/app/components/02-communication/02-output-event-directive/click-stop.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Output, EventEmitter, HostListener } from '@angular/core';
+import { Directive, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
@@ -7,10 +7,6 @@ import { Directive, ElementRef, Output, EventEmitter, HostListener } from '@angu
 export class ClickStopDirective {
   // tslint:disable-next-line:no-output-rename
   @Output('click.stop') clickStopEvent = new EventEmitter();
-
-  constructor(
-    private element: ElementRef
-  ) { }
 
   @HostListener('click', ['$event'])
   onClick(event) {
