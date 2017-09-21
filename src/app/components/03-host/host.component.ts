ab5a50b8411896a75bd2d198a8a6d86ec106f1b3
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-09-21 12:32:08 +0300

    updated host demo

--- src/app/components/03-host/host.component.ts
+++ src/app/components/03-host/host.component.ts
@@ -1,21 +1,26 @@
 import { Component, HostBinding, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-host',
+  selector: 'app-host', // <-- This is a host.
   templateUrl: './host.component.html',
   styleUrls: ['./host.component.css'],
 })
 export class HostComponent {
-  @HostBinding('class') attrClass = 'headingClass';
+  @HostBinding('class')
+  attrClass = 'headingClass';
 
-  @HostListener('click') clicked() {
+  @HostListener('click')
+  clicked() {
     console.log('click event on host element');
   }
 
-  @HostListener('mouseenter', ['$event']) enter(event: Event) {
+  @HostListener('mouseenter', ['$event'])
+  enter(event: Event) {
     console.log('mouseenter event on host element');
   }
-  @HostListener('mouseleave',  ['$event']) leave(event: Event) {
+
+  @HostListener('mouseleave', ['$event'])
+  leave(event: Event) {
     console.log('mouseleave event on host element');
   }
 }
