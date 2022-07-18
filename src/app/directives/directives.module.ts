fc0441e77339d8e0252576b8d8ed847eda03da00
VZhyrytskiy <Vitaliy_Zhyrytskyy@epam.com>
2022-07-18 16:44:10 +0300

    feat: add structural directives demo and reorganize project

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomIfDirective, CustomForDirective, CarouselDirective } from './structural-directives';
+import { HostDirective, Underline1Directive, HostComponent, UnderlineComponent, OnlyNumberComponent, Underline2Directive, OnlyNumberDirective } from './attribute-directives/host-binding-host-listener';
 
-const dirs = [CustomIfDirective, CustomForDirective, CarouselDirective];
+const dirs = [CustomIfDirective, HostDirective, CustomForDirective, Underline1Directive, Underline2Directive, OnlyNumberDirective, CarouselDirective];
+const comps = [HostComponent, UnderlineComponent, OnlyNumberComponent];
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [...dirs],
-  exports: [...dirs]
+  declarations: [...comps, ...dirs],
+  exports: [...comps, ...dirs]
 })
 export class DirectivesModule { }
